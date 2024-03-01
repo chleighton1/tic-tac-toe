a6c11c19bbe015400ee5fde79eb781001a0bda79
Charles Leighton <chleighton1@gmail.com>
2024-03-01 16:55:23 -0500

    Initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,16 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        "dark-navy": "#1A2A33",
+        "semi-dark-navy": "#1F3641",
+        silver: "#A8BFC9",
+        "silver-hover": "#DBE8ED",
+        "light-blue": "#31C3BD",
+        "light-blue-hover": "#65E9E4",
+        "light-yellow": "#F2B137",
+        "light-yellow-hover": "#FFC860",
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
